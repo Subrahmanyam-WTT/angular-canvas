7cce63c753b18802e6400aa7a50766f37ef8ca8f
Subrahmanyam-WTT <subrahmanyam.a@walkingtree.tech>
2019-10-18 18:30:49 +0530

    1 file modified, 3 added

---
+++ app/match-element/match-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-match-element',
+  templateUrl: './match-element.component.html',
+  styleUrls: ['./match-element.component.css']
+})
+export class MatchElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
